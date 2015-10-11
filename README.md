4a29717f86cb7ba58ab4c034705b2ec8022f1e91
Florian Weber <florian.weber.dd@icloud.com>
2015-10-11 12:02:10 +0200

    cs fix

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 webmail-client
 ==============
-
-A Symfony project created on October 11, 2015, 10:27 am.
--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
- * PageController
+ * PageController.
  *
  * @author Florian Weber <florian.weber.de@icloud.com>
  */
